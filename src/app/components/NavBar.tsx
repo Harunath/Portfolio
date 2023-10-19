2f3e041cb992f94a5e9a8fd66d082c70c1dbbf64
harunath <harunath04@gmail.com>
2023-10-19 12:21:04 +0530

    navbar ui

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -3,12 +3,10 @@ import React from 'react'
 
 function NavBar() {
   return (
-    <div className='absolute top-0 w-screen flex items-center justify-around sm:justify-normal gap-0 lg:gap-x-10 h-10 px-2 text-lg font-bold bg-black bg-opacity-10'>
-      <div className='shrink grow hidden sm:block'/>
+    <div className='absolute top-0 w-screen flex items-center justify-around sm:justify-center gap-0 gap-x-10 h-10 px-2 text-lg text-white font-bold bg-black bg-opacity-80'>
       <Link href={'/'}>Home</Link>
       <Link href={'/projects'}>Projects</Link>
       <Link href={'/contact'}>Contact me</Link>
-      <div className='shrink grow  hidden sm:block'/>
     </div>
   )
 }
